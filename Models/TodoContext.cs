c6441aeeb299e40bc1af2d2e90749a90a035721c
johnyohanyoon <johnyoondev@gmail.com>
2026-01-19 23:49:09 -0800

    Add indexes for query optimization

--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -5,9 +5,25 @@ namespace TodoApi.Models;
 public class TodoContext : DbContext
 {
     public TodoContext(DbContextOptions<TodoContext> options)
-        : base(options)
-    {
-    }
+        : base(options) { }
 
     public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Index on IsComplete for filtering
+        modelBuilder
+            .Entity<TodoItem>()
+            .HasIndex(t => t.IsComplete)
+            .HasDatabaseName("IX_TodoItems_IsComplete");
+
+        // Index on Name for searching
+        modelBuilder.Entity<TodoItem>().HasIndex(t => t.Name).HasDatabaseName("IX_TodoItems_Name");
+
+        // Composite index for filtering + pagination
+        modelBuilder
+            .Entity<TodoItem>()
+            .HasIndex(t => new { t.IsComplete, t.Id })
+            .HasDatabaseName("IX_TodoItems_IsComplete_Id");
+    }
 }
